2c83b1f471732a0a4a1194778dcfdcd2a356e969
Hugo Sousa <hugosousa42@proton.me>
2025-02-02 23:28:44 +0000

    Creating the MVC project.

---
+++ RequestMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RequestMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
